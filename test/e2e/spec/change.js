eee5feffe5f905f6fd0dcfb405cdc89ede250f18
Chungsub Kim <chungsub.kim@purpleworks.co.kr>
2015-01-05 22:40:13 +0900

    update test

--- test/e2e/spec/change.js
+++ test/e2e/spec/change.js
@@ -6,13 +6,16 @@ describe('change test', function() {
 
   it('change view = change title to "change.html"', function() {
     browser.get('/change.html#/change');
+    browser.sleep(1000);
     expect(browser.executeScript('return document.title;')).toEqual('change.html');
   });
 
   it('change view again = change title is still "Default Title"', function() {
     browser.get('/change.html#/change');
+    browser.sleep(1000);
     expect(browser.executeScript('return document.title;')).toEqual('change.html');
     browser.get('/change.html#/');
+    browser.sleep(1000);
     expect(browser.executeScript('return document.title;')).toEqual('Default Title');
   });
 });
